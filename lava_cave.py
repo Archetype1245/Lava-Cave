05197b450501ecb3fe5142b4609a2045e359df06
Archetype1245 <timpippin1245@gmail.com>
2025-08-05 18:05:29 -0500

    Added new channel

--- lava_cave.py
+++ lava_cave.py
@@ -8,7 +8,7 @@ import os
 GITHUB_USERNAME = "Archetype1245"
 REPO_NAME = "Lava-Cave"
 GITHUB_BASE_URL = f"https://{GITHUB_USERNAME}.github.io/{REPO_NAME}"
-ALLOWED_CHANNELS = {928807944487841842, 1352712382769664010}  # Replace with your channel's ID (an integer)
+ALLOWED_CHANNELS = {928807944487841842, 1352712382769664010, 1402388158196355142}  # Replace with your channel's ID (an integer)
 VIEW_TIMEOUT = None
 
 # ---------------------------
